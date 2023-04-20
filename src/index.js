e1afa6583ebab5f48f219d735bf60bd9f89e8f8a
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-04-20 20:43:15 +0200

    Added Zodiak table using react and react-bootrstrap for styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+// import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function ZodiakRow({signName, dates}){
+    return (
+        <tr>
+            <td>{signName}</td>
+            <td>{dates}</td>
+        </tr>
+    )
+}
+function ZodiakTable(){
+    return(
+            <Table bordered striped hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Sign of Zodiak</th>
+                        <th>Dates</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <ZodiakRow signName='♈ Aries (Ram)' dates='March 21–April 19'/>
+                    <ZodiakRow signName='♉ Taurus (Bull)' dates='April 20–May 20'/>
+                    <ZodiakRow signName='♊ Gemini (Twins)' dates='May 21–June 21'/>
+                    <ZodiakRow signName='♋ Cancer (Crab)' dates='June 22–July 22'/>
+                    <ZodiakRow signName='♌ Leo (Lion)' dates='July 23–August 22'/>
+                    <ZodiakRow signName='♍ Virgo (Virgin)' dates='August 23–September 22'/>
+                    <ZodiakRow signName='♎ Libra (Balance)' dates='September 23–October 23'/>
+                    <ZodiakRow signName='♏ Scorpius (Scorpion)' dates='October 24–November 21'/>
+                    <ZodiakRow signName='♐ Sagittarius (Archer)' dates='November 22–December 21'/>
+                    <ZodiakRow signName='♑ Capricornus (Goat)' dates='December 22–January 19'/>
+                    <ZodiakRow signName='♒ Aquarius (Water Bearer)' dates='January 20–February 18'/>
+                    <ZodiakRow signName='♓ Pisces (Fish)' dates='February 19–March 20'/>
+                </tbody>
+            </Table>
+        )
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById('root')).render(<ZodiakTable/>);
+// ReactDOM.render(<ZodiakTable/>, document.querySelector('#root'))
